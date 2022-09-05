7dbbfb36ee6cac827f9041eb0e0a0dbc2727cd5e
Lastzlo <z.last.zlo@gmail.com>
2022-09-06 00:26:03 +0300

    fix/ add application-test.properties

--- src/test/java/com/example/springMailSender/SpringMailSenderApplicationTests.java
+++ src/test/java/com/example/springMailSender/SpringMailSenderApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.springMailSender;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("/application-test.properties")
 class SpringMailSenderApplicationTests {
 
 	@Test
